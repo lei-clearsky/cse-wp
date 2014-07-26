5321a94b73c8efcaf5587c1ca18d1930c35aaea4
lei-clearsky <sportzhulei@gmail.com>
2014-07-26 11:54:05 -0400

    added catalog.php page and related files

--- functions.php
+++ functions.php
@@ -98,7 +98,10 @@ function college_scripts() {
 	wp_enqueue_style( 'college-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'college-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'college-modernizr', get_template_directory_uri() . '/js/modernizr.custom.js', array(), '20120206', true );
+	wp_enqueue_script( 'college-expand', get_template_directory_uri() . '/js/catalog-expand.js', array('jquery'), '20120206', true );
 	wp_enqueue_script( 'college-application', get_template_directory_uri() . '/js/application.js', array('jquery'), '20120206', true );
+
 
 	wp_enqueue_script( 'college-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
